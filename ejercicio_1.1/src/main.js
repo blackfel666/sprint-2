523513f4706f5eff531e4968cf16247d9b950195
blackfel666 <fv10644@gmail.com>
2025-10-20 20:58:42 +0200

    implement callback functions and asynchronous greeting in exercise 1.3

--- ejercicio_1.1/src/main.js
+++ ejercicio_1.1/src/main.js
@@ -118,3 +118,47 @@ const clacificacio = parOimpar(nums)
 console.log(clacificacio)
 
 /*Exercici 1.3 nivel 1*/
+
+function procesar (nombre, callback) {
+    if (typeof callback === 'function'){
+        callback(nombre);
+    } else {
+        console.error("Error: El segon paràmetre no és una funció de callback.")
+    }
+}
+procesar("irene","pedro")
+
+/*Exercici 1.3 nivel 1*/
+
+const suma = ( a, b ) => { return a + b }
+
+function calculadora (num1, num2, callback2){
+    return callback2 (num1, num2)
+}
+
+let resultatSuma = calculadora(5, 7, suma)
+console.log (`la suma de 5 y 7 es: ${resultatSuma}`)
+
+
+/*Exercici 1.3 nivel 2*/
+
+function esperarSaludar (nom, retraso) {
+    setTimeout(() => { console.log(`¡Hola, ${nom}!`)}, retraso)
+}
+
+const nombreUsuario = "Juanito";
+const tiempoEspera = 2000;
+
+esperarSaludar(nombreUsuario, tiempoEspera)
+
+console.log()
+
+/*Exercici 1.3 nivel 2*/
+
+
+
+
+
+
+
+
