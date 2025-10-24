781ee37b49a892dd1ebe655960652fa84e65f74d
blackfel666 <fv10644@gmail.com>
2025-10-24 18:28:55 +0200

    add styles and implement promises with async/await in exercise 1.7

--- ejercicio_1.1/src/7_promises_async_await.js
+++ ejercicio_1.1/src/7_promises_async_await.js
@@ -2,13 +2,54 @@
 
 /*Exercici 1.7 nivel 1*/
 
-const holaMon = () => { return new Promise((resolve, reject) => {
+const holaMon = () => { return new Promise((resolve) => {
     
     const tempEspera = 2000
     
-    setTimeout(() => { resolve('Hola, mon') }, tempEspera);
-})
+    setTimeout(() => { resolve('Hola, mon') }, tempEspera)
+})}
+
+/*Exercici 1.7 nivel 1*/
+
+holaMon () .then ( missatge => { console.log(`${missatge}`)})
+
+/*Exercici 1.7 nivel 1*/
+
+const promiseReject = (input) => { return new Promise((resolve, reject) => {
+    
+    const tempEspera = 2000
+    
+    setTimeout(() => { 
+
+        if (input === 'hola') { 
+            resolve (`input correcte`)
+        } else {
+            reject ( new Error(`input incorrecte`))
+        } 
+    }, tempEspera )
+    
+    })}
+
+    const asyncAwait = async () => { 
+        const mess = await holaMon();
+        console.log(mess)
+        return mess;
+    }
+
+/*Exercici 1.7 nivel 2*/
+
+const asyncAwaitErrors = async(promessFn) =>{
+    try {
+        const mess = await promessFn()
+        return mess
+    } catch (error) {
+        console.error("Error capturat")
+        throw error
+        }
 }
 
-holaMon()
-    .then( missatge => { console.log(`${missatge}`)})
+    /*Exercici 1.7 nivel 3*/
+const promessa = new Promise (resolve => setTimeout (() => resolve('resultat 1'), 2000))
+const promessa2 = new Promise (resolve => setTimeout (() => resolve('resultat 2'), 3000))
+
+Promise.all ([promessa, promessa2]) .then(resultats =>{console.log(`tot resolt`, resultats)})
